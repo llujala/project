d397ccecd474cde626ba435a913f95ff1d3fc152
Jamie Charlesworth <info@jamiecharlesworth.com>
2016-05-09 17:57:55 +0700

    Add files via upload

---
+++ js/flickity.js
@@ -0,0 +1,16 @@
+
+  $('.main-carousel-slideshow').flickity({
+    wrapAround: true,
+    autoPlay: 3000,
+    pauseAutoPlayOnHover: true,
+    prevNextButtons: true,
+    cellAlign: 'center'
+  });
+  $('.why-choose-us').flickity({
+    wrapAround: false,
+    contain: true,
+    autoPlay: false,
+    pauseAutoPlayOnHover: false,
+    prevNextButtons: false,
+    cellAlign: 'left'
+  });
